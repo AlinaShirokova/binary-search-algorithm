9546384f3eae7a83d99a7a14b65b2c0687a2e614
Alina S. Shirokova <shirokova.a.s@mail.ru>
2023-12-08 16:25:35 +0300

    Binary search algorithm

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int target = 67;
+        int[] nums = new int[]{4, 6, 7, 8, 9, 13, 67, 300, 670}; //an array of integers nums which is sorted in ascending order
+        System.out.println("Result: ");
+        System.out.println(BinarySearch.search(nums, target));
+    }
+}
